46e5d791ce2c93b9935dd994557373a3ef3c8505
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-23 05:13:45 -0700

    User adds a task to todo list: createElement, textContent, className, appendChild, addEventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("script.js connected")
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl =  document.querySelector("#tasks-to-do");
+
+var createTaskHandler = () => {
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
+    var textContentVal = prompt("What should you name the task?");
+    taskItemEl.textContent = textContentVal;
+    
+    tasksToDoEl.appendChild(taskItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
