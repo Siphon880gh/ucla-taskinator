709f8f0068000a1fdb9792acffeee7d661234214
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-27 01:38:16 -0700

    User can add task name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl =  document.querySelector("#tasks-to-do");
 
-var createTaskHandler = () => {
+var createTaskHandler = (event) => {
+    event.preventDefault();
+
+    // var textContentVal = prompt("What should you name the task?");
+    // taskItemEl.textContent = textContentVal;
+    var inputNameEl = document.querySelector("input[name='task-name']")
+    var taskNameInputVal = inputNameEl.value;
+
+    var taskTypeSelected = document.querySelector("select[name='task-type']").value;
+
+    if(taskNameInputVal.length===0) {
+        alert("Error: Enter a name for the task");
+        inputNameEl.focus();
+        return false;
+    }
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
-    var textContentVal = prompt("What should you name the task?");
-    taskItemEl.textContent = textContentVal;
+    taskItemEl.innerHTML = `<h3 class="task-name">${taskNameInputVal}</h3><span class="task-type">${taskTypeSelected}</span>`;
     
     tasksToDoEl.appendChild(taskItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
